c00f1735749772a59062ad37370aebe455ae0f12
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-02 02:49:59 -0600

    crear entidad para representar libros en las peticiones

---
+++ src/books/entities/book.entity.ts
@@ -0,0 +1,22 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Author } from '../../authors/entities/author.entity';
+
+@ObjectType()
+export class Book {
+  @Field(() => Int, { description: 'id del libro' })
+  id: number;
+
+  @Field(() => String, { description: 'nombre del libro' })
+  name: string;
+
+  @Field(() => String, { description: 'descripción del libro' })
+  description: string;
+
+  @Field(() => Date, { description: 'fecha de creación del libro' })
+  created_at: Date;
+  @Field(() => Date, { description: 'facha de actualización del libro' })
+  updated_at: Date;
+
+  @Field(() => [Author], { description: 'autores del libro' })
+  authors: Author[];
+}
