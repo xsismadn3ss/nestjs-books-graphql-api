c91bc4440fc07c902d88a1318e2a752136bd2dc2
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-02 02:46:43 -0600

    modificar campo para que sea opcional

--- src/authors/dto/create-author.input.ts
+++ src/authors/dto/create-author.input.ts
@@ -2,6 +2,6 @@ import { InputType, Field } from '@nestjs/graphql';
 
 @InputType()
 export class CreateAuthorInput {
-  @Field(() => String, { description: 'nombre del author' })
+  @Field(() => String, { description: 'nombre del author', nullable: true })
   name: string;
 }
