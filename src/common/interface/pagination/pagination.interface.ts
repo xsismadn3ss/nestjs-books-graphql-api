df30ac7b8ba3664460c1750824afa5c3ac2133f5
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-01 23:32:48 -0600

    agregar validadores de clase en los esquemas

--- src/common/interface/pagination/pagination.interface.ts
+++ src/common/interface/pagination/pagination.interface.ts
@@ -1,13 +1,17 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { Max, Min } from 'class-validator';
 
 @InputType()
 export class PaginationInput {
   @Field(() => Int, { description: 'pagina actual', defaultValue: 1 })
+  @Min(1)
   page: number = 1;
 
   @Field(() => Int, {
     description: 'cantidad de registros por pagina',
     defaultValue: 10,
   })
+  @Min(1)
+  @Max(100)
   size: number = 10;
 }
