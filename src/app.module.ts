d1c4447ad479938b7d828959cf6316aae2935ee3
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-02 02:44:51 -0600

    modificar importaciones del modulo principal, agregar graphql en el modulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
+import { BooksModule } from './books/books.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [AuthorsModule],
+  imports: [
+    AuthorsModule,
+    BooksModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql',
+      graphiql: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
