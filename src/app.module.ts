71c28a8a0e48d170a165f0840ef0751dc3550725
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-01 23:34:28 -0600

    importar el modulo de autores en el modulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [],
+  imports: [AuthorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
