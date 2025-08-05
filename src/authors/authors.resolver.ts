4e209067790356bbae786374689fdd26898bf86a
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-05 14:46:22 -0600

    actualizar resolver para manejar errores

--- src/authors/authors.resolver.ts
+++ src/authors/authors.resolver.ts
@@ -13,8 +13,10 @@ import { UpdateAuthorInput } from './dto/update-author.input';
 import { PaginationInput } from '../common/interface/pagination/pagination.interface';
 import { PaginationPipe } from '../common/pipe/pagination/pagination.pipe';
 import { PubSubService } from '../common/service/pub-sub/pub-sub.service';
-import { UseInterceptors } from '@nestjs/common';
+import { NotFoundException, UseInterceptors } from '@nestjs/common';
 import { LoggingInterceptor } from '../common/interceptor/logging/logging.interceptor';
+import { AuthorResult } from './entities/author-result.entity';
+import { Message } from '../common/entity/message.entity';
 
 @UseInterceptors(LoggingInterceptor)
 @Resolver(() => Author)
@@ -46,9 +48,16 @@ export class AuthorsResolver {
     return this.authorsService.findAll(paginationInput, name);
   }
 
-  @Query(() => Author, { name: 'author' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.authorsService.findOne(id);
+  @Query(() => AuthorResult, { name: 'author' })
+  async findOne(@Args('id', { type: () => Int }) id: number) {
+    try {
+      return await this.authorsService.findOne(id);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return new Message({ message: 'author not found' });
+      }
+      throw error
+    }
   }
 
   @Mutation(() => Author)
@@ -58,9 +67,17 @@ export class AuthorsResolver {
     return this.authorsService.update(updateAuthorInput);
   }
 
-  @Mutation(() => Author)
-  removeAuthor(@Args('id', { type: () => Int }) id: number) {
-    return this.authorsService.remove(id);
+  @Mutation(() => Message)
+  async removeAuthor(@Args('id', { type: () => Int }) id: number) {
+    try {
+      await this.authorsService.remove(id)
+      return new Message({ message: "author deleted" })
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return new Message({ message: 'author not found' });
+      }
+      throw error
+    }
   }
 
   @Subscription(() => Author)
