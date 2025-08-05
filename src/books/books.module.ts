fc22a0e01f46a5f8abd0d27df535db502c58d954
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-04 22:38:27 -0600

    Actualizar imports del modulo para resolver dependencias

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksResolver } from './books.resolver';
 import { PrismaModule } from '../common/service/prisma/prisma.module';
+import { PubSubModule } from 'src/common/service/pub-sub/pub-sub.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PubSubModule],
   providers: [BooksResolver, BooksService],
 })
-export class BooksModule {}
+export class BooksModule { }
