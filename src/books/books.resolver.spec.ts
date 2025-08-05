7c19e0e32bf28836eaa28f6eca380c6f66005daa
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-04 22:37:07 -0600

    actualizar pruevas unitarias para el modulo de usuarios

--- src/books/books.resolver.spec.ts
+++ src/books/books.resolver.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksResolver } from './books.resolver';
 import { BooksService } from './books.service';
+import { PubSubModule } from '../common/service/pub-sub/pub-sub.module';
+import { PrismaModule } from '../common/service/prisma/prisma.module';
 
 describe('BooksResolver', () => {
   let resolver: BooksResolver;
@@ -8,6 +10,7 @@ describe('BooksResolver', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [BooksResolver, BooksService],
+      imports:[PubSubModule, PrismaModule],
     }).compile();
 
     resolver = module.get<BooksResolver>(BooksResolver);
--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BooksService } from './books.service';
+import { PrismaModule } from '../common/service/prisma/prisma.module';
 
 describe('BooksService', () => {
   let service: BooksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [BooksService],
     }).compile();
 
