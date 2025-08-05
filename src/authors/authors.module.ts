1b149c267e6e0387894be2e8de015a6b0a374b19
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-04 21:27:58 -0600

    actualizar modulo e inyecta servicio de pub sub

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { AuthorsResolver } from './authors.resolver';
 import { PrismaModule } from '../common/service/prisma/prisma.module';
+import { PubSub } from 'graphql-subscriptions';
 
 @Module({
   imports: [PrismaModule],
-  providers: [AuthorsResolver, AuthorsService],
+  providers: [AuthorsResolver, AuthorsService, PubSub],
 })
 export class AuthorsModule {}
