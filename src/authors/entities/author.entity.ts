d27ed2f7f97c8eb5c44e059afb99cc65c4fb1044
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-05 14:31:28 -0600

    añadir constructor para la clase

--- src/authors/entities/author.entity.ts
+++ src/authors/entities/author.entity.ts
@@ -13,4 +13,21 @@ export class Author {
 
   @Field(() => Date, { description: 'fecha de actualizacion del author' })
   updated_at: Date;
+
+  constructor({
+    id,
+    name,
+    created_at,
+    updated_at,
+  }: {
+    id: number;
+    name: string;
+    created_at: Date;
+    updated_at: Date;
+  }) {
+    this.id = id;
+    this.name = name;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+  }
 }
