46fc6384811846ac00c6b1315e1ef534e6fc3748
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-08-05 14:46:52 -0600

    añadir constructor a la clase Book

--- src/books/entities/book.entity.ts
+++ src/books/entities/book.entity.ts
@@ -19,4 +19,20 @@ export class Book {
 
   @Field(() => [Author], { description: 'autores del libro' })
   authors: Author[];
+
+  constructor({
+    id,
+    name,
+    description,
+    created_at,
+    updated_at,
+    authors,
+  }: Book) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.created_at = created_at;
+    this.updated_at = updated_at;
+    this.authors = authors;
+  }
 }
